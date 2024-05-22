c4eea4f6b4d22940d05e5b5ec7c05a2d5a563b1f
Ishita Gupta <148317513+ishita48@users.noreply.github.com>
2024-05-22 18:33:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Components/Component/TicTacToe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
